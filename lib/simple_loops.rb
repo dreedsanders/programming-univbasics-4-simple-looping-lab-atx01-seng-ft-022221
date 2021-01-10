5b8dce210423eaee5e308039e484de62b56dde3e
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-10 19:54:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,15 +11,15 @@ end
 puts loop_message_five_times(message_0)
 
 message_1 = "Hello Moon."
+num_1 = 0
 
-def loop_message_n_times(message)
-  num = 0 
+def loop_message_n_times(message)(num)
    while (num) < 5 
    puts message
    num += 1 
  end
  end
  
- puts loop_message_n_times(message_1)
+ puts loop_message_n_times(message_1)(num_1)
  
   
