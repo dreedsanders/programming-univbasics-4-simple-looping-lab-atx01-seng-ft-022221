89f0f021ba41f5317d7c8b249198eb541f4c4703
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-10 21:12:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -51,7 +51,7 @@ numstring = 5, 4, 3, 2, 1
 
 def return_string_array(array)
   count = 0 
-  while count < array.length 
+  while count < array.length do
   puts array[count].to_s
     count += 1 
   end
