010ab1ab0d23b84ad2d7d74b33cafcd5128b026d
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-10 20:24:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,4 +10,15 @@
 end
 
 output_array_elements(quote)
+
+ def output_array(array)
+   counter = 0 
+   
+   while array [counter] do 
+   puts array[counter]
+   counter += 1 
+ end
+ end
+ 
+output_array(quote)
   
