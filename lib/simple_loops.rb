563631a48377494e7324df5e642f9aaf3583ebc8
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-10 20:21:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,38 +1,3 @@
-message_0 = "Hello World."
-
-def loop_message_five_times(message)
-  puts message
-   puts message
-    puts message
-     puts message
-      puts message
-end
-
-puts loop_message_five_times(message_0)
-
-message_1 = "Hello Moon."
-message_2 = "Hello Red Balloon"
-
-def loop_message_n_times(message)
-  num = 0 
-   while (num) < 5 
-   puts message
-   num += 1 
- end
- end
- 
- puts loop_message_n_times(message_1)
- 
- def loop_message_n_times(message)
-  num = 0 
-   while (num) < 10 
-   puts message
-   num += 1 
- end
- end
- 
- puts loop_message_n_times(message_2)
- 
  quote = ["Be yourself;", "everyone else is already taken.", "-Oscar Wilde"]
  
  def output_array(array)
