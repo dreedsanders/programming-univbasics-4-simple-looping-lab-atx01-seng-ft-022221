621462603f93062f64a104004fe74e88a3ff3d4f
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-10 20:25:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,16 +1,5 @@
  quote = ["Be yourself;", "everyone else is already taken.", "-Oscar Wilde"]
  
- def output_array_elements(array)
-  counter = 0
- 
-  while array[counter] do
-    puts array[counter]
-    counter += 1
-  end
-end
-
-output_array_elements(quote)
-
  def output_array(array)
    counter = 0 
    
