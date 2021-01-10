10b4ebe996d5bd66ff03c14f48353ee2dd19e142
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-10 19:53:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,15 @@
+message_0 = "Hello World."
+
+def loop_message_five_times(message)
+  puts message
+   puts message
+    puts message
+     puts message
+      puts message
+end
+
+puts loop_message_five_times(message_0)
+
 message_1 = "Hello Moon."
 
 def loop_message_n_times(message)
